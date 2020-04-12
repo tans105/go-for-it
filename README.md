a949cdf2e9a76eecf09c74c14b9aaa372cdfe698
Tanmay Awasthi <awata02@c02yvflnlvcg.bbrouter>
2020-04-12 13:23:21 +0530

    Creation of templates and basic routing between signup & signin page

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # go-for-it
-Demo web-application with Sign-up, Sign In implementation using session, cookie based authentication 
+Demo web-application with Sign-up, Sign In implementation using session, cookie based authentication on Go(Golang).
+
+# Setting it up
+- Setup golang on local (https://golang.org/doc/install)
+- Once setup, clone the repo
+- run `go build`. This will generate an executable.
--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*gohtml"))
+}
+
 func main() {
 	http.HandleFunc("/", home)
+	http.HandleFunc("/signup", signup)
+
 	http.ListenAndServe(port, nil)
 }
 
 func home(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, "Welcome to the homepage")
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func signup(w http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(w, "signup.gohtml", nil)
 }
